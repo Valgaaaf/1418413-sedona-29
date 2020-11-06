51ceb69801dba45af9ef25b771436cac452a335d
Oleg <babotina88@gmail.com>
2020-11-06 21:43:18 +0300

    Итог 06.11.2020
    1. чекбокс поправил
    2. js сделал

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const search_form = document.querySelector(".search-form");
+
 const search_button = document.querySelector(".search-button");
+
+const date_in = document.querySelector("#in");
+
+const date_out = document.querySelector("#out");
+
+const adults = document.querySelector("#adults");
+
+const kids = document.querySelector("#kids");
 
 document.addEventListener("DOMContentLoaded", () => { 
     search_form.classList.add("search-form-off");
@@ -9,3 +18,12 @@ search_button.addEventListener("click", () => {
     search_form.classList.toggle("search-form-off");
 });
 
+search_form.addEventListener("submit", function (evt) {
+if (!date_in.value || !date_out.value || !kids.value || !adults.value) {
+evt.preventDefault();
+console.log ("Введите данные");
+search_form.classList.remove("search-form-error");
+search_form.offsetWidth = search_form.offsetWidth;
+search_form.classList.add("search-form-error");
+}
+});
